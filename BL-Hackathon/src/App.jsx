ca5601f2757c6f13bb501bb7d6d6c0a943f0be8f
Blakc Coffeee <dharunkarthikeyan19@gmail.com>
2026-01-08 18:25:14 +0530

    updating new homepage and removing old Home.jsx

--- BL-Hackathon/src/App.jsx
+++ BL-Hackathon/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
+import { HomePage } from "./pages/HomePage";
+import { MyProfiles } from "./pages/MyProfiles";
 import { SoloProfileCreate } from "./pages/SoloProfileCreate";
 import { SoloProfileView } from "./pages/SoloProfileView";
 import { TeamProfileCreate } from "./pages/TeamProfileCreate";
@@ -7,10 +8,12 @@ import { TeamProfileCreate } from "./pages/TeamProfileCreate";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/my-profiles" element={<MyProfiles />} />
       <Route path="/create-profile" element={<SoloProfileCreate />} />
       <Route path="/profile/:id" element={<SoloProfileView />} />
       <Route path="/create-team" element={<TeamProfileCreate />} />
+      <Route path="/team/:id" element={<SoloProfileView />} />
     </Routes>
   );
 }
